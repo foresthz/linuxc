91063daca11304dd5012fa532b816b1427408193
cq45 <lina@tom.com>
2014-09-17 20:05:47 +0800

    point to struct

--- c/comp/struct.c
+++ c/comp/struct.c
@@ -8,9 +8,14 @@ struct stu {
 int main()
 {
   printf("hello world\n");
-  struct stu a;
+  struct stu a, *b;
+  
   a.name="Tom";
   a.age=11;
   printf("age is : %d", a.age);
+  b=&a;
+  (*b).age = 12;
+  // 指针对象必须加括号，否则编译报错?
+  printf("new age is : %d", (*b).age);
   return 0;
 }
